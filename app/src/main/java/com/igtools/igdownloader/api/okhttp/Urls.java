4a54a97f1dcdff473487f6c14c3a681d668736a5
moo611 <lnpjjds@gmail.com>
2022-08-18 21:06:35 +0800

    fix

--- app/src/main/java/com/igtools/igdownloader/api/okhttp/Urls.java
+++ app/src/main/java/com/igtools/igdownloader/api/okhttp/Urls.java
@@ -2,19 +2,19 @@ package com.igtools.igdownloader.api.okhttp;
 
 public class Urls {
 
-    //public static String BASE_URL_PY = "http://192.168.0.100:3000/";
+    //public static String BASE_URL_PY = "http://192.168.0.100:3000";
 
-    //public static String BASE_URL_PY = "http://192.168.100.38:3000/";
+    //public static String BASE_URL_PY = "http://192.168.100.38:3000";
 
-    public static String BASE_URL_PY = "http://35.90.136.197:3000/";
+    public static String BASE_URL_PY = "http://35.90.136.197:3000";
 
-    public static String BASE_URL_JAVA = "http://192.168.100.209:8080/";
+    public static String BASE_URL_JAVA = "http://192.168.100.209:8080";
 
-    public static String SHORT_CODE = BASE_URL_PY + "api/mediainfo";
+    public static String SHORT_CODE = BASE_URL_PY + "/api/mediainfo";
 
-    public static String USER_NAME = BASE_URL_PY + "api/userinfo";
+    public static String USER_NAME = BASE_URL_PY + "/api/userinfo";
 
-    public static String USER_TAG = BASE_URL_PY + "api/taginfo";
+    public static String USER_TAG = BASE_URL_PY + "/api/taginfo";
 
     public static String LOGIN = BASE_URL_JAVA + "/auth/login";
 
